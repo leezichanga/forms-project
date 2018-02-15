14537706c11a0257f81bc5d3b21384e87fb471b9
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-02-15 12:05:40 +0300

    Add js scripts

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+      $(document).ready(function() {
+        $("#blanks form").submit(function() {
+          $(".person1").append("blah blah");
+          $(".person2").append("blah blah");
+          $(".animal").append("blah blah");
+          $(".exclamation").append("blah blah");
+          $(".verb").append("blah blah");
+          $(".noun").append("blah blah");
+
+          $("#story").show();
+        });
+      });
+
+            $(document).ready(function() {
+              $("#blanks form").submit(function(event) {
+                $(".person1").append("blah blah");
+                $(".person2").append("blah blah");
+                $(".animal").append("blah blah");
+                $(".exclamation").append("blah blah");
+                $(".verb").append("blah blah");
+                $(".noun").append("blah blah");
+
+                $("#story").show();
+
+                event.preventDefault();
+              });
+            });
+$("input#person1").val();
